02453c09ac9aac808e8c8c003ccdb4db999ca4f7
Ayush <ayushgoyal0808@gmail.com>
2025-04-27 19:04:33 +0530

    [Ayush Goyal] ADD: created implementations of interfaces in service layer.

---
+++ src/main/java/com/bridgelabz/real_estate_roi_tracker/service/ExpenseServiceImpl.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.real_estate_roi_tracker.service;
+
+public class ExpenseServiceImpl implements IExpenseService{
+
+}
---
+++ src/main/java/com/bridgelabz/real_estate_roi_tracker/service/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.real_estate_roi_tracker.service;
+
+public class UserServiceImpl implements IUserService {
+
+}
